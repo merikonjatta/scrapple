3feba3ea54e03e44c77d8fe1e74502d7469606c1
Shinya Maeyama <merikonjatta@gmail.com>
2012-03-07 17:35:45 +0900

    Remove edit/write actions from raw handler

--- webapp/plugins/raw_handler/raw_handler.rb
+++ webapp/plugins/raw_handler/raw_handler.rb
@@ -5,14 +5,4 @@ class RawHandler < Compound::Handler
 		File.open(path){ |f| f.read }
 	end
 
-	def edit(path)
-		headers "Content-Type" => "text/plain"
-		"Editing:\n" + File.open(path){ |f| f.read }
-	end
-
-	def write(path)
-		headers "Content-Type" => "text/plain"
-		"Wrote #{path} with:\n\n#{params[:content]}"
-	end
-
 end
